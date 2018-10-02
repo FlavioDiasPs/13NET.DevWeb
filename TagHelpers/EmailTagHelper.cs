0469b8d0fa5945417d257a45090a0d2d36752815
Flavio Pêgas <FlavioDias_4@hotmail.com>
2018-10-01 22:41:00 -0300

    TagHelpers

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula01.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailToMommy { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var address = $"{MailToMommy}@Manja.Rola";
+
+            output.PreContent.SetContent("Eu sei que ");
+            output.Attributes.SetAttribute("href", $"mailto:{address}");
+            output.PostContent.SetContent(". É verdade esse teg.");
+            output.Content.SetContent(address);
+        }
+    }
+
+
+}
---
+++ TagHelpers/LabelRequiredTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula01.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = ForAttributeName)]
+    public class LabelRequiredTagHelper : LabelTagHelper
+    {
+        private const string ForAttributeName = "*";
+
+        public LabelRequiredTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+
+            if(For.Metadata.IsRequired)
+            {
+                var sup = new TagBuilder("sub");
+                sup.InnerHtml.AppendHtml("<div>Hello coloca o valor");
+                output.Content.AppendHtml(sup);
+            }
+        }
+    }
+}
